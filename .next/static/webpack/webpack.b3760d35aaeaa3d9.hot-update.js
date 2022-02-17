0631a882b57ee5392529ea3e9dc3f992dd8e68d2
tomi <tomiwolfman@gmail.com>
2022-02-17 13:34:50 -0300

    second commit

--- .next/static/webpack/webpack.b3760d35aaeaa3d9.hot-update.js
+++ .next/static/webpack/webpack.b3760d35aaeaa3d9.hot-update.js
@@ -33,7 +33,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6d1531f47d65beef"; }
+/******/ 	__webpack_require__.h = function() { return "ce760d2ff64547fe"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
