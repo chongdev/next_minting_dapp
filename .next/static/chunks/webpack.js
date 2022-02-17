8dcb59d082e074306b85c5d7a2d759a2a493d85f
tomi <tomiwolfman@gmail.com>
2022-02-17 12:54:58 -0300

    second commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c09f1ad078df2274"; }
+/******/ 		__webpack_require__.h = function() { return "bef7bff6d9e7c6e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
